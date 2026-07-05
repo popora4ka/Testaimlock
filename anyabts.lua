22943a3de5aac20cba3f874ba672efb424f2ce93
popora4ka <134146902+popora4ka@users.noreply.github.com>
2026-07-05 16:49:03 +0300

    Update anyabts.lua

--- anyabts.lua
+++ anyabts.lua
@@ -1,6 +1,6 @@
 -- MM2 Murderer Aim Lock for Innocent/Sheriff
 local shared = odh_shared_plugins
-local my_own_section = shared.AddSection("Best MM2 Aim Lock")
+local my_section = shared.AddSection("Best MM2 Aim Lock")
 
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
